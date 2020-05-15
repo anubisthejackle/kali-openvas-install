caf99b4c8e943e4b8326eb8e32ae6a3d1302c178
Travis Weston <tweston2013@gmail.com>
2020-05-15 15:24:53 -0400

    Fix the sed syntax so it doesn't break the sudoers file

--- root/9-sudoers.sh
+++ root/9-sudoers.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-sed -i -e '/secure_path/ s[=.*[&:/opt/gvm/sbin[' /etc/sudoers
+sed -i -e '/secure_path/ s[=\(.*\)\("\)[=\1/opt/gvm/sbin\2[' /etc/sudoers
 
-cat <<< EOF >> /etc/sudoers
-
-### Allow the user running ospd-openvas, to launch openvas with root permissions
-gvm ALL = NOPASSWD: /opt/gvm/sbin/openvas
-gvm ALL = NOPASSWD: /opt/gvm/sbin/gsad
-
-EOF
-
+echo "### Allow the user running ospd-openvas, to launch openvas with root permissions" >> /etc/sudoers
+echo "gvm ALL = NOPASSWD: /opt/gvm/sbin/openvas" >> /etc/sudoers
+echo "gvm ALL = NOPASSWD: /opt/gvm/sbin/gsad" >> /etc/sudoers
