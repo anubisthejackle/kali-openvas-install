96c8415dc0cc2a873c3c2ef85f4f7ec45c343b72
Travis Weston <tweston2013@gmail.com>
2020-05-15 16:28:32 -0400

    Add the flag to not error on deprecation warnings to misc and nasl CMakeLists

--- gvm/7-install-openvas.sh
+++ gvm/7-install-openvas.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 cd openvas
-export PKG_CONFIG_PATH=/opt/gvm/lib/pkgconfig:$PKG_CONFIG_PATH
-mkdir build
+
+# Because of quirks with Kali 2020.1, we need to change the make config
+# We need to change CMakeLists.txt in the misc and nasl directories
+
+# Original Line: set (CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -Werror")
+# New Line: set (CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -Werror -Wno-error=deprecated-declaration")
+sed -i -e "s/\(-Werror\)\(.*\)/\2 -Wno-error=deprecated-declarations\2/" misc/CMakeLists.txt
+sed -i -e "s/\(-Werror\)\(.*\)/\2 -Wno-error=deprecated-declarations\2/" nasl/CMakeLists.txt
+
 cd build/
 cmake -DCMAKE_INSTALL_PREFIX=/opt/gvm ..
 make
